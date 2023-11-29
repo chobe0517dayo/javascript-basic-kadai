172063d7e01c9a5d0c4067d8523fb720d85bfe7b
chobe0517dayo <148618112+chobe0517dayo@users.noreply.github.com>
2023-11-29 12:34:04 +0900

    kadai_005アップロード

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,10 +1,12 @@
 // 変数の宣言
 let bodyWeight;
 let bodyHeight;
+let bodyBmi;
 
 // 値の代入
 bodyWeight = '68'
 bodyHeight = '1.7'
+bodyBmi = bodyWeight / (bodyHeight * bodyHeight);
 
 // 四則演算
-console.log(bodyWeight / bodyHeight * bodyHeight);
+console.log(bodyBmi)
